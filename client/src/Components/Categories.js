1a4908327e3a6890ff9e6e8d87066759a63b7f1b
Vishwa Sandaruwan <2021cs170@stu.ucsc.cmb.ac.lk>
2023-08-24 20:37:18 +0530

    Index Page Updated
    added right side card list

--- client/src/Components/Categories.js
+++ client/src/Components/Categories.js
@@ -1,12 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBars,
-  faTv,
-  faShirt,
-  faHouseChimney,
-  faBurger,
-  faFootball,
-} from "@fortawesome/free-solid-svg-icons";
 import CategoryList from "./CategoryList";
 
 export default function Categories() {
