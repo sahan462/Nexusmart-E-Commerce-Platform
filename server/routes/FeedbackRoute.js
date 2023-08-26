3496343af163575a5707594081158486216f61b3
damitha sahan <sahanchandrasena462@gmail.com>
2023-08-27 05:07:00 +0530

    working on feedback controller modifications

--- server/routes/FeedbackRoute.js
+++ server/routes/FeedbackRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const {viewFeedbacks, addFeedback, updateFeedback, deleteFeedback} = require('../controllers/FeedbackController');
+//const {verifySigning} = require("../middleware/auth");
 
-const {viewFeedbacks, addFeedback} = require('../controllers/FeedbackController');
-
-router.post('/', addFeedback);
-router.get('/',viewFeedbacks);
+router.get('/:itemId', viewFeedbacks);
+router.post('/newfeedback/:itemId', addFeedback);
+router.put('/update', updateFeedback);
+router.delete('/delete', deleteFeedback);
 
 module.exports = router;
