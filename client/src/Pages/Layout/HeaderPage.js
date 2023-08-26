51837dd67d709aea6f500bc279fd9f892635c95c
Vishwa Sandaruwan <2021cs170@stu.ucsc.cmb.ac.lk>
2023-08-26 18:10:20 +0530

    implemented seller registration

--- client/src/Pages/Layout/HeaderPage.js
+++ client/src/Pages/Layout/HeaderPage.js
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../../AuthContext";
 import { useContext } from "react";
 
+// import { logo } from "./../../assets/logo.png";
+
 function HeaderPage() {
   const { userData } = useContext(UserContext);
 
@@ -24,6 +26,7 @@ function HeaderPage() {
               d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z"
             />
           </svg>
+          {/* <img className="w-6 h-6" alt="Logo" src={logo} /> */}
           <span className="font-bold">NexusMart</span>
         </Link>
       </div>
@@ -76,9 +79,11 @@ function HeaderPage() {
         )}
         {/* Seller Dashboard  */}
         {!!userData && userData.role === "seller" && (
-          <div className="h-10 px-10 bg-primary text-white text-sm font-semibold  border border-none rounded-lg flex justify-center items-center hover:bg-primary_hover">
-            <Link className="">My Selling</Link>
-          </div>
+          <Link>
+            <div className="h-10 px-10 bg-primary text-white text-sm font-semibold  border border-none rounded-lg flex justify-center items-center hover:bg-primary_hover">
+              My Selling
+            </div>
+          </Link>
         )}
         {/* Profile  */}
         {!!userData && (
