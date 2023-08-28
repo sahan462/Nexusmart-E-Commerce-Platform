536de65e57bb74d7c01e41bbd03ccb8bc09c32b6
damitha sahan <sahanchandrasena462@gmail.com>
2023-08-28 15:54:41 +0530

    login/logout done

--- server/routes/UserRegisterLogRoute.js
+++ server/routes/UserRegisterLogRoute.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 const {registerBuyer} = require('../controllers/Auth/BuyerRegisterController');
 const {registerSeller} = require('../controllers/Auth/SellerRegisterController');
-const {login} = require('../controllers/Auth/UserLoginController');
+const {login, logout} = require('../controllers/Auth/UserLoginLogoutController');
 
 router.post('/register_buyer',registerBuyer);
 router.post('/register_seller', registerSeller);
-router.post('/login',login);
+router.post('/login', login);
+router.post('/logout', logout);
 
 module.exports = router;
