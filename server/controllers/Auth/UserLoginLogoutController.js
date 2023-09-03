c716454f1e4fda2a96933a3eb5f40bf5708fc719
damitha sahan <sahanchandrasena462@gmail.com>
2023-09-03 20:10:53 +0530

    pullreq commit

--- server/controllers/Auth/UserLoginLogoutController.js
+++ server/controllers/Auth/UserLoginLogoutController.js
@@ -22,10 +22,8 @@ const login = async (req, res) => {
             //creating jwt
             jwt.sign(userData, jwtSecret, { expiresIn: '1h' }, (err, token) => {
                 if (err) throw err;
-                res
-                    .header('x-auth-token', token)
-                    .cookie('token', token, { httpOnly: true, maxAge: 3600000 })
-                    .json({ name: userDoc.name, email: userDoc.email, role: userDoc.role });
+                res.header('x-auth-token', token);
+                res.cookie('token', token, { httpOnly: true, maxAge: 3600000 }).json({ name: userDoc.name, token:token,email: userDoc.email, role: userDoc.role });
             });
         } else {
             res.status(422).json("Access Denied");
