c7be7384b2f172a5412e354283adce8b26def0fe
dhanushkacc <136559309+dhanushkacc@users.noreply.github.com>
2023-09-06 19:46:35 +0530

    Create full backend and frontend dor customer supports

--- server/index.js
+++ server/index.js
@@ -5,13 +5,13 @@ const app = express();
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
-
-const regLog = require('./routes/UserRegisterLogRoute');
-const item = require('./routes/ItemRoute');
-const cart = require('./routes/CartRoute');
-const order = require('./routes/OrderRoute');
-const feedback = require('./routes/FeedbackRoute');
-const auth = require('./middleware/auth')
+const regLog = require("./routes/UserRegisterLogRoute");
+const item = require("./routes/ItemRoute");
+const cart = require("./routes/CartRoute");
+const order = require("./routes/OrderRoute");
+const feedback = require("./routes/FeedbackRoute");
+const auth = require("./middleware/auth");
+const issue = require("./routes/CustomerIssueRoute");
 
 app.use(
   cors({
@@ -27,18 +27,17 @@ connect(process.env.MONGO_URL)
   .then(() => console.log("connected to mongodb..."))
   .catch(() => console.log("Could not connect to mongodb..."));
 
-
-app.use('/auth', regLog);
-app.use('/items', item);
-app.use('/cart', auth.verifySignin, cart);
-app.use('/feedback', feedback);
-app.use('/order', order);
+app.use("/auth", regLog);
+app.use("/items", item);
+app.use("/cart", auth.verifySignin, cart);
+app.use("/feedback", feedback);
+app.use("/order", order);
+app.use("/issue", issue);
 
 // for testing - TODO :: remove after
 app.get("/test", (req, res) => {
   res.json("Working");
 });
-
 
 const port = 5000;
 app.listen(port, () => {
