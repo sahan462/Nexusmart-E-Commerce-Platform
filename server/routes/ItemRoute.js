3ddef75e882c46f3ff761409e34ea05875924fe4
damitha sahan <sahanchandrasena462@gmail.com>
2023-09-15 07:37:18 +0530

    userid

--- server/routes/ItemRoute.js
+++ server/routes/ItemRoute.js
@@ -4,7 +4,7 @@ const {addItem, viewItems, changeItemProp, deleteItem} = require('../controllers
 const {verifySeller, verifySignin} = require('../middleware/auth')
 
 router.post('/',[verifySignin, verifySeller], addItem);
-router.get('/', itemController.viewItems);
+router.get('/', viewItems);
 router.get('/getItems', [verifySignin, verifySeller], viewItems)
 router.put('/:itemId',[verifySignin, verifySeller], changeItemProp);
 router.delete('/:itemId',[verifySignin, verifySeller], deleteItem);
